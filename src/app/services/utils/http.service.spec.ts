1240bb5760e03fc959a0915010c56ec8c4d2dde4
Mönnich Daniel, ENT-BD-PDD <Daniel.Moennich@swisscom.com>
2016-04-13 14:10:42 +0200

    Added tests for http.service read.

--- src/app/services/utils/http.service.spec.ts
+++ src/app/services/utils/http.service.spec.ts
@@ -1,19 +1,19 @@
 import { expect } from 'chai';
 import servicesModule from '../services.module.ts';
 import {IHttpUtilService} from './http.service.ts';
-import {httpService} from '../services.module.ts';
+import {httpService as httpServiceName} from '../services.module.ts';
 import AppConfig from '../../app.config.ts';
 
 describe('http.service', () => {
 
     let httpBackend: ng.IHttpBackendService;
-    let http_service: IHttpUtilService;
+    let httpService: IHttpUtilService;
 
     beforeEach(() => {
         angular.mock.module(servicesModule);
-        angular.mock.inject(['$httpBackend', httpService, ($httpBackend: ng.IHttpBackendService, httpService: IHttpUtilService) => {
+        angular.mock.inject(['$httpBackend', httpServiceName, ($httpBackend: ng.IHttpBackendService, _httpService_: IHttpUtilService) => {
             httpBackend = $httpBackend;
-            http_service = httpService;
+            httpService = _httpService_;
         }]);
     });
 
@@ -22,8 +22,50 @@ describe('http.service', () => {
         httpBackend.verifyNoOutstandingRequest();
     });
 
-    it('should use base API from APPConfig', () => {
-        expect(http_service.getBackendUrl()).to.equal(AppConfig.ENV.API_URL);
+    it('should use base API URL from APPConfig', () => {
+        expect(httpService.getBackendUrl()).to.equal(AppConfig.ENV.API_URL);
+    });
+
+    describe('read', () => {
+
+        it('should send GET request to provided URL', () => {
+            httpBackend.expect('GET', `${AppConfig.ENV.API_URL}/test`).respond({});
+            httpService.read('/test');
+            httpBackend.flush();
+        });
+
+        it('should send GET request with given params', () => {
+            const params: any = {
+                name: 'Jimbo',
+                age: 55
+            };
+            httpBackend.expect('GET', `${AppConfig.ENV.API_URL}/test?age=55&name=Jimbo`).respond({});
+            httpService.read('/test', params);
+            httpBackend.flush();
+        });
+
+        it('should return a promise providing the response data', () => {
+            httpBackend.expect('GET', `${AppConfig.ENV.API_URL}/user/1`).respond({name: 'Jimbo'});
+            httpService.read('/user/1').then((resp) => {
+                expect(resp.name).to.equal('Jimbo');
+            });
+            httpBackend.flush();
+        });
+
+        it('should return a promise providing full response in case of an error', () => {
+            httpBackend.expect('GET', `${AppConfig.ENV.API_URL}/error`).respond(
+                500,
+                {error: 123},
+                {'THE-Token': '555'}
+            );
+            httpService.read('/error').then(undefined, (resp) => {
+                expect(resp.status).to.equal(500);
+                expect(resp.data.error).to.equal(123);
+                expect(resp.headers('THE-TOKEN')).to.equal('555');
+            });
+            httpBackend.flush();
+        });
+
     });
 
 });
